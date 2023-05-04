21aa443025419a684e81208bce2f09d4347fb92c
Nhi Vu <vuthuynhi05@gmail.com>
2023-05-04 12:30:14 -0500

    changed header to center and reactive

--- src/consts.ts
+++ src/consts.ts
@@ -2,8 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Nhi Vu's Personal Website";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Xin Chào!";
 export const GITHUB_LINK = "https://github.com/Tuilanhi";
 export const TWITTER_LINK = "https://twitter.com/TUILANH1";
-export const LINKED_LINK = "https://www.linkedin.com/in/nhi-vu-868a2b234/";
+export const LINKEDIN_LINK = "https://www.linkedin.com/in/nhi-vu-868a2b234/";
 export const NAME = "Nhi Vu";
