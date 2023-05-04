2c6de884caada968b2b3155c6f10290806d8bec9
Nhi Vu <vuthuynhi05@gmail.com>
2023-05-04 02:00:20 -0500

    added initial files

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Nhi Vu's Personal Website";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const GITHUB_LINK = "https://github.com/Tuilanhi";
+export const TWITTER_LINK = "https://twitter.com/TUILANH1";
+export const LINKED_LINK = "https://www.linkedin.com/in/nhi-vu-868a2b234/";
+export const NAME = "Nhi Vu";
