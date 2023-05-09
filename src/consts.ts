e24ff14424e529db653c00bf171c6c75e926b6bc
Nhi <vuthuynhi05@gmail.com>
2023-05-09 01:27:36 -0500

    merge with working-branch

--- src/consts.ts
+++
@@ -1,9 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Nhi Vu's Personal Website";
-export const SITE_DESCRIPTION = "Xin Chào";
-export const GITHUB_LINK = "https://github.com/Tuilanhi";
-export const TWITTER_LINK = "https://twitter.com/TUILANH1";
-export const LINKEDIN_LINK = "https://www.linkedin.com/in/nhi-vu-868a2b234/";
-export const NAME = "Nhi Vu";
